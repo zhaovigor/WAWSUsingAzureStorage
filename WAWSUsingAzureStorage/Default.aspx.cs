fbe121fa8a2ff818acd03d1c54469b9e3d6e7bc5
unknown <wzhao@WZHAO-T430.fareast.corp.microsoft.com>
2013-05-29 07:32:47 +0800

    initial version

---
+++ WAWSUsingAzureStorage/Default.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.StorageClient;
+using System.Configuration;
+
+namespace WAWSUsingAzureStorage
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        private CloudBlobContainer blobContainer;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            blobContainer = InitContainer();
+            ListBlobsInContainer();
+        }
+
+        protected void BtnUpload_Click(object sender, EventArgs e)
+        {
+            if (FileUploadCtrl.HasFile)
+            {
+                this.SaveImage(
+                  Guid.NewGuid().ToString(),
+                  FileUploadCtrl.FileName,
+                  FileUploadCtrl.PostedFile.ContentType,
+                  FileUploadCtrl.FileBytes
+                );
+            }
+            ListBlobsInContainer();
+        }
+
+        private void SaveImage(string id, string fileName, string contentType, byte[] data)
+        {
+            // Create a blob in container and upload image bytes to it
+            var blob = blobContainer.GetBlobReference(fileName);
+
+            blob.Properties.ContentType = contentType;
+
+            // Create some metadata for this image
+            var metadata = new NameValueCollection();
+            metadata["Id"] = id;
+            metadata["Filename"] = fileName;
+            
+            // Add and commit metadata to blob
+            blob.Metadata.Add(metadata);
+            blob.UploadByteArray(data);
+        }
+
+        private CloudBlobContainer InitContainer()
+        {
+            // Get a handle on account, create a blob service client and get container proxy
+
+            StorageCredentialsAccountAndKey credentials = new StorageCredentialsAccountAndKey(ConfigurationManager.AppSettings["storageAccount"], ConfigurationManager.AppSettings["storageKey"]);
+            CloudStorageAccount storageAccount = new CloudStorageAccount(credentials, false);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            var container = blobClient.GetContainerReference(ConfigurationManager.AppSettings["containerName"]);
+
+            try
+            {
+                container.CreateIfNotExist();
+            }
+            catch (StorageClientException sce)
+            { 
+                if (sce.ErrorCode.Equals(StorageErrorCode.ContainerAlreadyExists))
+                {
+                    //nothing, already there;
+                } else {
+                    throw sce;
+                }
+            }
+
+            var permissions = container.GetPermissions();
+            permissions.PublicAccess = BlobContainerPublicAccessType.Container;
+            container.SetPermissions(permissions);
+
+            return container;
+        }
+
+        private void ListBlobsInContainer()
+        {
+            BlobsLiteral.Text = "";
+             //List blobs and directories in this container hierarchically (which is the default listing).
+            foreach (var blobItem in blobContainer.ListBlobs())
+            {
+                BlobsLiteral.Text += blobItem.Uri;
+                BlobsLiteral.Text += "</br>";
+            }
+            
+        }
+    }
+}
---
+++ WAWSUsingAzureStorage/obj/Release/Package/PackageTmp/Default.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WAWSUsingAzureStorage.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:FileUpload ID="FileUploadCtrl" runat="server" />
+        <asp:Button ID="BtnUpload" runat="server" OnClick="BtnUpload_Click" Text="Upload" />
+    </div>
+        <div>
+
+            <asp:Literal ID="BlobsLiteral" runat="server"></asp:Literal>
+
+        </div>
+        <div>
+            <a href="WAWSUsingAzureStorage.zip" title="SourceCode">SourceCode</a>
+        </div>
+    </form>
+</body>
+</html>
